59406b70c2e72fee8c72f5c9bc5d91838e806639
Stephanie Trimboli <satrimboli@gmail.com>
2018-06-21 03:28:44 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <post 
+     image="Ji9qebSpSlSbnsWRZnDX__MG_9520.jpg"
+     title="Are we out of the woods yet?" 
+     author="Bob Loblaw"
+     date="August 6, 2015"
+     :tags="['environment', 'swift', 'fungus']"
+     content= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non consequat ligula. Aenean efficitur tellus eu egestas mollis. Aenean ultrices sodales purus, a sodales ex imperdiet ut. Curabitur molestie eget diam id feugiat. Sed dignissim metus ut mauris varius lacinia. Fusce non felis quis libero finibus convallis. Suspendisse tincidunt nibh et placerat bibendum. Etiam ornare sit amet felis id commodo. Nunc id luctus metus. Aenean in felis et sapien volutpat dapibus quis quis massa. Donec sodales, arcu facilisis imperdiet fringilla, lectus lacus viverra orci, at malesuada mi lorem a ligula. Nullam eu lorem finibus, tincidunt nisi vitae, ornare lorem. Maecenas id semper dui. Vivamus in felis mi.
+
+Etiam a tellus eu lorem scelerisque laoreet. Donec nec vehicula diam. Sed risus arcu, egestas eu urna sed, rhoncus accumsan odio. Vivamus vitae congue odio. Donec a commodo eros, eu eleifend odio. Donec interdum interdum sapien, eget viverra turpis venenatis id. Nullam ultricies, ligula a dictum blandit, lorem metus pellentesque nibh, id fringilla purus sapien eu ligula. Aenean bibendum lobortis vestibulum. Phasellus hendrerit sollicitudin ante, placerat imperdiet mi. Nam ultricies dolor elementum aliquet lobortis. Phasellus tincidunt imperdiet ipsum, in venenatis lorem tempus eget. Nam in dapibus diam, id convallis tortor. Nunc in ultrices tellus, eget consequat nisl.
+
+Vestibulum porttitor nulla id velit tristique, sit amet fringilla elit euismod. Duis urna magna, condimentum quis orci eu, tristique posuere justo. Nullam fermentum neque ligula, sit amet convallis mi feugiat eu. Morbi sollicitudin sem nec tellus venenatis, a faucibus lectus finibus. Ut eu aliquet nibh. Cras tristique nulla at enim condimentum convallis. Nam eu interdum nunc, quis venenatis urna. Nulla mattis ut nibh id ornare. Etiam sed orci a sem efficitur malesuada. Sed mattis elementum urna a fringilla. Suspendisse potenti.
+
+Phasellus gravida sit amet tellus sit amet sagittis. Nam ac tempor lectus, at iaculis nulla. Nam eu posuere eros. In iaculis diam a turpis pretium faucibus. Etiam volutpat feugiat nisl non lacinia. Ut aliquam elementum sem, aliquet convallis arcu accumsan ultrices. Donec porttitor tortor sit amet vulputate finibus. Morbi erat risus, laoreet vel ex tristique, pretium fermentum mauris. Cras vel ante nulla.
+
+Sed lectus ligula, tincidunt vitae ullamcorper quis, pulvinar quis tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ultrices posuere risus eu dignissim. Praesent efficitur, tortor sit amet rhoncus dapibus, erat elit dignissim erat, a varius mauris turpis at neque. Suspendisse quis aliquet neque, non aliquet leo. Suspendisse purus quam, cursus quis ligula ut, placerat tincidunt elit. Fusce a mauris id justo pellentesque pellentesque et at tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique a purus et tincidunt. Suspendisse dictum neque id sagittis consectetur. Maecenas ultricies viverra eros id euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+    
+    />
+  </div>
+</template>
+
+<script>
+import post from './components/post.vue'
+
+export default {
+  name: 'app',
+  components: {
+    post
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/post.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="post">
+    <img class="post_image" v-bind:src="getAssetPath(image)" />
+    <div class="post_icon--edit"></div>
+    <h1 class="post_title">{{title}}</h1>
+    <div class="post_details">
+      <div class="post_author"></div>
+      <div class="post_date">{{date}}</div>
+      <ul class="post_tags">
+        <li v-for="tag in tags" class="post_tag">{{tag}}</li>
+      </ul>
+    </div>
+    <div class="post_body">
+      {{content}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post',
+  props: {
+    title: String,
+    author: String,
+    date: String,
+    tags: Object,
+    content: String,
+    pullQuote: String,
+    image: String
+  },
+  data: function() {
+    return {
+      isEditing: false
+    }
+  },
+  methods: {
+    getAssetPath: function(asset) {
+      return require('../assets/' + asset)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.post_icon--edit {
+  width: 42px;
+  height: 42px;
+  background-image: url(../assets/pencil.svg);
+  background-repeat: no-repeat;
+  background-color: #f3e13b;
+  background-position: 50%;
+}
+.post_image {
+  max-width: 100%;
+}
+
+.post_title {
+  background-color: #fff;
+  mix-blend-mode: screen;
+  font-size: 50px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
